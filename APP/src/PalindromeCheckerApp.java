1e09db4552166f6b9b75cc32da06ddc977623dc3
balajiakash6 <balajiakash06@gmail.com>
2026-02-25 15:09:41 +0530

    UC2 - Print a Hardcoded Palindrome Result

--- APP/src/PalindromeCheckerApp.java
+++ APP/src/PalindromeCheckerApp.java
@@ -1,13 +1,9 @@
 public class PalindromeCheckerApp {
     public static void main(String args[]){
-        System.out.println("======================================");
         System.out.println(" Welcome to Palindrome Checker App ");
-        System.out.println("======================================");
         System.out.println("Application Name : Palindrome Checker App");
         System.out.println("Version          : 1.0");
         System.out.println("Description      : This application checks whether");
-        System.out.println("                   a given string is a palindrome.");
-        System.out.println("======================================");
         System.out.println("Application started successfully.");
     }
 }
